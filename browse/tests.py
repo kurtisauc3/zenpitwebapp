6082bf50d5bd3162060783b4a9cc33e4c784fc17
Kurtis Trainor <kurtistrainor@gmail.com>
2018-07-02 20:54:48 -0400

    updated branch

--- browse/tests.py
+++
@@ -1,3 +0,0 @@
-from django.test import TestCase
-
-# If you insist
--- browse/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-# any '' urls bing over to browse/templates/search/index.html
-urlpatterns = [
-    path(r'', views.index, name='browse'),
-]
