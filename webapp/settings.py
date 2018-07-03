0c6d8b7ceeb20d5cb5796f87700ba991d0e761df
Kurtis Trainor <kurtistrainor@gmail.com>
2018-07-03 00:23:41 -0400

    last try 2

--- webapp/settings.py
+++ webapp/settings.py
@@ -51,9 +51,6 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                "django.core.context_processors.i18n",
-                "django.core.context_processors.media",
-                "django.core.context_processors.static",
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
